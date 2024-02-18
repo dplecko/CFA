ac9c6367a000342bd5f88105852994a68d6afd11
Drago Plecko <drago@Dragos-MBP.lan>
2024-02-17 20:37:38 -0500

    add unit tests for new functions

---
+++ tests/testthat/test-fair-decisions.R
@@ -0,0 +1,36 @@
+
+test_that("fair_decisions works", {
+
+  with_seed(201, {
+    data <- data_gen(100, add_z = TRUE)
+  })
+
+  data$y <- as.integer(data$y) - 1L
+  data$d <- rbinom(nrow(data), size = 1, prob = 0.5)
+  vars <- c("y", "x", "w", "z", "d")
+
+  expect_setequal(colnames(data), vars)
+
+  fd.nms <- c("d_fcb", "delta_fcb", "data", "delta", "delta_sign",
+              "delta_transform",
+              "xgb_mod", "xgb_params", "x0", "x1", "model", "X", "W", "Z",
+              "Y", "D", "cl", "method", "tune_params", "nboot1", "nboot2")
+
+  # run fair_predictions
+  fd <- fair_decisions(
+    data = data, X = "x", Z = "z", W = "w", Y = "y", D = "d", x0 = 0, x1 = 1
+  )
+
+  autoplot(fd, type = "decision")
+  autoplot(fd, type = "delta")
+  autoplot(fd, type = "benefit_fairness")
+
+  expect_type(fd, "list")
+  expect_named(fd, fd.nms, ignore.order = TRUE)
+
+  fd_preds <- predict(fd, newdata = data[, c("x", "w", "z")], budget = 0.2)
+  expect_type(fd_preds, "list")
+  autoplot(fd_preds, type = "decision")
+  autoplot(fd_preds, type = "delta")
+  autoplot(fd_preds, type = "benefit_fairness")
+})
