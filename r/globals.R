d769a2fe66d965d3c95efa8f31e82d9793d52be5
Drago Plecko <drago@Dragos-MBP.lan>
2024-02-18 20:07:26 -0500

    fix global bindings

---
+++ r/globals.R
@@ -0,0 +1,4 @@
+
+utils::globalVariables(c("bin", "V1", "X", "V2", "lmbd", "bce", "bce_sd",
+                         "acc", "acc_sd", "auc", "auc_sd", "mse", "mse_sd",
+                         "Measure", "Value", "StdDev", "value", "Y", "x"))
